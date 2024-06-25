266ead50f716e7ca08ae4ac9dc9f35decb8b31dd
pardana <aditria.pardana@yahoo.co.id>
2024-06-25 16:13:28 +0700

    use library

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,15 +6,17 @@ import {
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import CheckBox from '@react-native-community/checkbox';
 
 
 function App(): React.JSX.Element {
-
+  const [done, setDone] = useState(false)
 
   return (
     <View>
-      {/* <Text>App</Text> */}
+      <Text>App</Text>
       <Icon name={'trash-can'} size={43} color='tomato' />
+      <CheckBox value={done} onValueChange={() => setDone(!done)} />
     </View>
   )
 }
